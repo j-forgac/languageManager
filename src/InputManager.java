65dd3476504109fffd00e7f1e578378027ef7bae
Jan Forgac <jan.forgac@educanet.cz>
2020-11-27 22:59:11 +0100

    singleton constructor added

--- src/InputManager.java
+++ src/InputManager.java
@@ -3,7 +3,10 @@ import java.util.Scanner;
 public enum InputManager {
     ME;
 
-    public static String loadInput(){
+    private InputManager() {
+    }
+
+    public static String loadInput() {
         System.out.println("zmenit jazyk?");
         Scanner sc = new Scanner(System.in);
         return sc.nextLine();
